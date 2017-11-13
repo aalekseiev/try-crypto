0c397055a8ad5763faa5f6dddd53473e7ae0716a
kseniia <alekseyev.anton@gmail.com>
2017-11-13 09:49:59 +0200

    initial commit

---
+++ src/main/java/com/example/trycrypto/TryCryptoApplication.java
@@ -0,0 +1,12 @@
+package com.example.trycrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryCryptoApplication.class, args);
+	}
+}
